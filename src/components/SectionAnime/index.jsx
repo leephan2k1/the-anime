307e2b8c5b0ff74d8eed0c32bbf0576043062e87
lee phan <pttl2k1@gmail.com>
2022-01-01 21:52:54 +0700

    setup section anime & install react content loader

--- src/components/SectionAnime/index.jsx
+++ src/components/SectionAnime/index.jsx
@@ -1,9 +1,7 @@
-import React from "react";
 import PropTypes from "prop-types";
-import Carousel from "../Carousel";
-
+import React from "react";
 import { sectionCarouselSettings } from "../../settings";
-
+import Carousel from "../Carousel";
 import "./styles.scss";
 
 function SectionAnime(props) {
@@ -11,7 +9,10 @@ function SectionAnime(props) {
 
   return (
     <div className="section w-full d-flex flex-column">
-      <h4 className="section__title">Anime mới cập nhật</h4>
+      <a href="#" className="section__title">
+        Anime mới cập nhật
+        <i className="fas fa-chevron-right"></i>
+      </a>
       <Carousel
         settings={sectionCarouselSettings}
         carouselType={"carouselSection"}
