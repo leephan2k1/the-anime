5b8316bb5ca0d574921f93ae07b4a6facf984823
lee phan <pttl2k1@gmail.com>
2022-01-03 03:07:23 +0700

    switch logic call api to app component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import "./App.scss";
 import NavBar from "./components/NavBar";
-import HomePage from "./pages/HomePage";
 import Anime from "./pages/Anime";
-
+import { Suspense, lazy, useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
+import ANIAPI from "@mattplays/aniapi";
+import aniListApi from "./api/aniListAPI";
+
+import { useDispatch } from "react-redux";
+import { addList } from "./components/Carousel/carouselSlice";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+
 function App() {
+  const API = new ANIAPI.API("DUMMY_JWT");
+  const [newAniList, setNewAniList] = useState([]);
+  const [suggestList, setSuggestList] = useState([]);
+  const dispatch = useDispatch();
+
+  //call api
+  useEffect(() => {
+    const fetchRamdomList = async () => {
+      try {
+        const response = await aniListApi.getRandom(200);
+        const listHasBanner = response.data.filter(
+          (e) => e.banner_image !== undefined
+        );
+        //store to redux
+        dispatch(addList(listHasBanner));
+
+        const dataFilter = await API.Anime.Get(
+          { year: 2021, season: 3 },
+          1,
+          10
+        );
+        //store to state
+        setNewAniList(dataFilter.data.documents);
+        const dataSuggest = await API.Anime.Get(
+          {
+            genres: ["Action", "Adventure", "Comedy"],
+            year: 2021,
+            status: [ANIAPI.ENUMS.AnimeStatus.FINISHED],
+          },
+          1,
+          10
+        );
+        //store to state
+        setSuggestList(dataSuggest.data.documents);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchRamdomList();
+  }, []);
+
   return (
     <div className="Anime-app d-flex flex-column">
+      {console.log("app render")}
       <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/anime" element={<Anime />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "5rem", fontSize: "10rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <HomePage newAniList={newAniList} suggestList={suggestList} />
+            }
+          />
+          <Route path="/anime" element={<Anime />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "5rem", fontSize: "10rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,55 +1,15 @@
-import ANIAPI from "@mattplays/aniapi";
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import aniListApi from "../../api/aniListAPI";
+import React from "react";
+import { useSelector } from "react-redux";
 import { randomList } from "../../app/selectors";
 import Carousel from "../../components/Carousel";
-import { addList } from "../../components/Carousel/carouselSlice";
 import Header from "../../components/Header";
 import SectionAnime from "../../components/SectionAnime";
 import SectionSeason from "../../components/SectionSeason";
 import { mainCarouselSettings } from "../../settings";
 
-export default function HomePage() {
-  const API = new ANIAPI.API("DUMMY_JWT");
+export default function HomePage(props) {
   const data = useSelector(randomList);
-  const dispatch = useDispatch();
-
-  const [newAniList, setNewAniList] = useState([]);
-
-  //call api
-  useEffect(() => {
-    const fetchRamdomList = async () => {
-      try {
-        const response = await aniListApi.getRandom(200);
-        const listHasBanner = response.data.filter(
-          (e) => e.banner_image !== undefined
-        );
-        const dataFilter = await API.Anime.Get(
-          { year: 2021, season: 3 },
-          1,
-          10
-        );
-        setNewAniList(dataFilter.data.documents);
-        // console.log(dataFilter.data.documents);
-        dispatch(addList(listHasBanner));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchRamdomList();
-  }, []);
-
-  //test filer
-  // useEffect(() => {
-  //   const fetchFilter = async () => {
-  //     try {
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchFilter();
-  // }, []);
+  const { newAniList, suggestList } = props;
 
   return (
     <div className="w-full h-full">
@@ -61,6 +21,7 @@ export default function HomePage() {
       />
       <SectionAnime title={"Anime mới cập nhật"} data={newAniList} />
       <SectionSeason />
+      <SectionAnime title={"Hôm nay xem gì?"} data={suggestList} />
     </div>
   );
 }
