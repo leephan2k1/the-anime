fcd4677fc94abc0121b42595ed6b8efde5b89084
lee phan <pttl2k1@gmail.com>
2022-01-18 21:56:44 +0700

    rm

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { homeList } from "../../app/selectors";
 import { mainCarouselSettings } from "../../settings";
-// import { Outlet } from "react-router-dom";
-import Carousel from "../../components/Carousel";
-import Header from "../../components/Header";
-import SectionAnime from "../../components/SectionAnime";
-import SectionCategory from "../../components/SectionCategory";
-import SectionSeason from "../../components/SectionSeason";
+import Carousel from "components/Carousel";
+import Header from "components/Header";
+import SectionAnime from "components/SectionAnime";
+import SectionCategory from "components/SectionCategory";
+import SectionSeason from "components/SectionSeason";
 
 import { browsePagePath } from "constants/path";
 
@@ -37,7 +36,6 @@ function HomePage(props) {
         title={"Thể loại"}
         target={`${browsePagePath}/category`}
       />
-      {/* <Outlet /> */}
     </div>
   );
 }
